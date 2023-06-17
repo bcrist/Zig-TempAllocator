c9f955915ada19fc168cb5de1d5be8eff773639e
Ben Crist <ben@magicmoremagic.com>
2023-06-17 11:11:45 -0500

    Update for build system changes

--- build.zig
+++ build.zig
@@ -1,13 +1,20 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    const mode = b.standardReleaseOptions();
+    const mode = b.standardOptimizeOption(.{});
 
-    const tests = b.addTest("tests.zig");
-    tests.setTarget(target);
-    tests.setBuildMode(mode);
+    _ = b.addModule("TempAllocator", .{
+        .source_file = .{ .path = "TempAllocator.zig" },
+    });
+
+    const tests = b.addTest(.{
+        .root_source_file = .{ .path = "tests.zig" },
+        .target = target,
+        .optimize = mode,
+    });
+    const run_tests = b.addRunArtifact(tests);
 
     const test_step = b.step("test", "Run all tests");
-    test_step.dependOn(&tests.step);
+    test_step.dependOn(&run_tests.step);
 }
