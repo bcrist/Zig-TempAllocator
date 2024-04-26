ba52f51d8692e59f533ff05954007096669dec81
Ben Crist <github@magicmoremagic.com>
2024-04-25 23:49:47 -0500

    fix tests, mark for zig 0.12

--- tests.zig
+++ tests.zig
@@ -2,7 +2,7 @@ test {
     var ta = try TempAllocator.init(1024 * 1024 * 1024 * 1024); // 1 TB
     defer ta.deinit();
 
-    try expectEqual(@as(usize, 0), ta.highWaterUsage());
+    try expectEqual(@as(usize, 0), ta.high_water_usage());
     try expectEqual(@as(usize, 0), ta.committed());
 
     var alloc: std.mem.Allocator = ta.allocator();
@@ -13,15 +13,15 @@ test {
 
     temp = try alloc.alloc(u8, 300000);
     try expectEqual(@as(usize, 300000), temp.len);
-    try expectEqual(@as(usize, 300017), ta.highWaterUsage());
+    try expectEqual(@as(usize, 300017), ta.high_water_usage());
     try expectEqual(@as(usize, 0x50000), ta.committed());
 
     for (temp) |*b| {
         b.* = 13;
     }
 
-    ta.reset();
-    try expectEqual(@as(usize, 0), ta.highWaterUsage());
+    ta.reset(.{});
+    try expectEqual(@as(usize, 0), ta.high_water_usage());
     try expectEqual(@as(usize, 0x50000), ta.committed());
 
     temp = try alloc.alloc(u8, 1000);
@@ -29,20 +29,20 @@ test {
     try expectEqual(@as(usize, 1000), snapshot);
 
     temp = try alloc.alloc(u8, 1000);
-    try expectEqual(@as(usize, 2000), ta.highWaterUsage());
-    ta.releaseToSnapshot(snapshot);
+    try expectEqual(@as(usize, 2000), ta.high_water_usage());
+    ta.release_to_snapshot(snapshot);
     try expectEqual(@as(usize, 1000), ta.snapshot());
-    try expectEqual(@as(usize, 2000), ta.highWaterUsage());
+    try expectEqual(@as(usize, 2000), ta.high_water_usage());
 
     var i: usize = 0;
     while (i < 1000) : (i += 1) {
         // std.debug.print("{}: estimate: {}\tcommitted: {}\n", .{ i, ta.usage_estimate, ta.committed() });
-        ta.reset();
+        ta.reset(.{});
     }
 
     try expectEqual(@as(usize, 0x30000), ta.committed());
 }
 
 const expectEqual = std.testing.expectEqual;
-const TempAllocator = @import("TempAllocator");
+const TempAllocator = @import("Temp_Allocator");
 const std = @import("std");
